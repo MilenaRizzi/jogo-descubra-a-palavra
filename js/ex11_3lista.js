3b714d4d90f6b2a32cab5cc5a437839753dc1412
MilenaRizzi <116590085+MilenaRizzi@users.noreply.github.com>
2024-10-14 19:15:29 -0300

    codigo para listagem de palavra

---
+++ js/ex11_3lista.js
@@ -0,0 +1,51 @@
+const tbPalavras = document.querySelector("table");
+const ckMostrar = document.querySelector("input[type='checkbox']");
+
+const mostrarTabela = () => {
+  if (localStorage.getItem("jogoPalavra") && localStorage.getItem("jogoDica")) {
+    const palavras = localStorage.getItem("jogoPalavra").split(";");
+    const dicas = localStorage.getItem("jogoDica").split(";");
+
+    for (let i = 0; i < palavras.length; i++) {
+      const linha = tbPalavras.insertRow(-1);
+
+      const col1 = linha.insertCell(0);
+      const col2 = linha.insertCell(1);
+      const col3 = linha.insertCell(2);
+
+      col1.innerText = palavras[i];
+      col2.innerText = dicas[i];
+      col3.innerHTML = "<i class='exclui' title='Excluir'>&#10008;</i>";
+    }
+
+    // Ocorre quando o checkbox é alterado. Exibe a lista se marcado, caso contrário recarrega a página
+    ckMostrar.addEventListener("change", () => {
+      ckMostrar.checked ? montarTabela() : window.location.reload();
+    });
+
+    tbPalavras.addEventListener("click", (e) => {
+      if (e.target.classList.contains("exclui")) {
+        const palavra =
+          e.target.parentElement.parentElement.children[0].innerText;
+
+        if (confirm(`Confirma Exclusão de Palavra:  "${palavra}"?`)) {
+          e.target.parentElement.parentElement.remove();
+
+          localStorage.removeItem("jogoPalavra");
+          localStorage.removeItem("jogoDica");
+
+          const palavras = [];
+          const dicas = [];
+
+          for (let i = 1; i < tbPalavras.rows.length; i++) {
+            palavras.push(tbPalavras.rows[i].cells[0].innerText);
+            dicas.push(tbPalavras.rows[i].cells[1].innerText);
+          }
+
+          localStorage.setItem("jogoPalavra", palavras.join(";"));
+          localStorage.setItem("jogoDica", dicas.join(";"));
+        }
+      }
+    });
+  }
+};
