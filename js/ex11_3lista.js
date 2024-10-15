f93482d5cce3819a9d775611292e238e09d5da8a
MilenaRizzi <116590085+MilenaRizzi@users.noreply.github.com>
2024-10-15 12:54:13 -0300

    correcao listagem

--- js/ex11_3lista.js
+++ js/ex11_3lista.js
@@ -1,8 +1,8 @@
 const tbPalavras = document.querySelector("table");
 const ckMostrar = document.querySelector("input[type='checkbox']");
 
-const mostrarTabela = () => {
-  if (localStorage.getItem("jogoPalavra") && localStorage.getItem("jogoDica")) {
+const montarTabela = () => {
+  if (localStorage.getItem("jogoPalavra")) {
     const palavras = localStorage.getItem("jogoPalavra").split(";");
     const dicas = localStorage.getItem("jogoDica").split(";");
 
@@ -17,35 +17,34 @@ const mostrarTabela = () => {
       col2.innerText = dicas[i];
       col3.innerHTML = "<i class='exclui' title='Excluir'>&#10008;</i>";
     }
-
-    // Ocorre quando o checkbox é alterado. Exibe a lista se marcado, caso contrário recarrega a página
-    ckMostrar.addEventListener("change", () => {
-      ckMostrar.checked ? montarTabela() : window.location.reload();
-    });
-
-    tbPalavras.addEventListener("click", (e) => {
-      if (e.target.classList.contains("exclui")) {
-        const palavra =
-          e.target.parentElement.parentElement.children[0].innerText;
-
-        if (confirm(`Confirma Exclusão de Palavra:  "${palavra}"?`)) {
-          e.target.parentElement.parentElement.remove();
-
-          localStorage.removeItem("jogoPalavra");
-          localStorage.removeItem("jogoDica");
-
-          const palavras = [];
-          const dicas = [];
-
-          for (let i = 1; i < tbPalavras.rows.length; i++) {
-            palavras.push(tbPalavras.rows[i].cells[0].innerText);
-            dicas.push(tbPalavras.rows[i].cells[1].innerText);
-          }
-
-          localStorage.setItem("jogoPalavra", palavras.join(";"));
-          localStorage.setItem("jogoDica", dicas.join(";"));
-        }
-      }
-    });
   }
 };
+
+// Ocorre quando o checkbox é alterado. Exibe a lista se marcado, caso contrário recarrega a página
+ckMostrar.addEventListener("change", () => {
+  ckMostrar.checked ? montarTabela() : window.location.reload();
+});
+
+tbPalavras.addEventListener("click", (e) => {
+  if (e.target.classList.contains("exclui")) {
+    const palavra = e.target.parentElement.parentElement.children[0].innerText;
+
+    if (confirm(`Confirma Exclusão de Palavra:  "${palavra}"?`)) {
+      e.target.parentElement.parentElement.remove();
+
+      localStorage.removeItem("jogoPalavra");
+      localStorage.removeItem("jogoDica");
+
+      const palavras = [];
+      const dicas = [];
+
+      for (let i = 1; i < tbPalavras.rows.length; i++) {
+        palavras.push(tbPalavras.rows[i].cells[0].innerText);
+        dicas.push(tbPalavras.rows[i].cells[1].innerText);
+      }
+
+      localStorage.setItem("jogoPalavra", palavras.join(";"));
+      localStorage.setItem("jogoDica", dicas.join(";"));
+    }
+  }
+});
